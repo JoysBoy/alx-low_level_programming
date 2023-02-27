6c0a6a0b5c0bbb04d6312b89e27aca0f8faff497
JoysBoy <tbgselamolela@gmail.com>
2023-02-27 05:01:31 -0800

    Dont swap horses in crossing a stream & his report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,19 +2,20 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-
 int _strlen(char *s)
 {
-	int longi = 0;
+	int i;
 
-	while (*s != '\0')
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		longi++;
-		s++;
+		i++;
 	}
 
-	return (longi);
+	return (i);
 }
