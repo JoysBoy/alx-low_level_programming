ce1d24a8b7d52c0cb9de80707ced69c73d64a0df
JoysBoy <tbgselamolela@gmail.com>
2023-03-09 06:56:36 -0800

    updates

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,29 +1,30 @@
+#include "main.h"
+
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be considered
- * identical, otherwise returns 0.
- * @s1: pointer to first string
- * @s2: pointer to second string (may contain wildcard '*')
- *
- * Return: 1 if strings are identical, 0 otherwise.
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	/* Base case */
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	/* Special case */
-    	if (*s2 == '*')
-	{
 	if (*s1 == '\0')
-		return (wildcmp(s1, s2 + 1));
-
-	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
 	}
 
-	/* General case */
-	if (*s1 == *s2)
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
 		return (wildcmp(s1 + 1, s2 + 1));
-
-   	return (0);
+	}
+	return (0);
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,32 @@
-#include <math.h>
+#include "main.h"
+
+int actual_prime(int n, int i);
 
 /**
- * is_prime_number - check if a number is a prime number
- * @n: the number to check
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
  *
- * Return: 1 if n is a prime number, 0 otherwise
+ * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	/* check for base cases */
-	if (n < 2)
+	if (n <= 1)
 		return (0);
-	else if (n == 2)
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
 		return (1);
-
-	/* loop through all numbers from 2 to sqrt(n) */
-	for (i = 2; i <= sqrt(n); i++)
-    {
-	/* if i divides n evenly, n is not a prime number */
-	if (n % i == 0)
+	if (n % i == 0 && i > 0)
 		return (0);
-    }
-
-   	/* if we get here, n is a prime number */
-	return (1);
+	return (actual_prime(n, i - 1));
 }
