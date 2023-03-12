be0ea8b205bada00778f8a130591710ba6983e6d
JoysBoy <tbgselamolela@gmail.com>
2023-03-12 00:04:58 -0800

    main.h

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* Prototypes for functions in file1.c */
+int function1(int arg1, char *arg2);
+void function2(void);
+
+/* Prototypes for functions in file2.c */
+int function3(int arg1);
+char *function4(char *arg2);
+
+#endif
