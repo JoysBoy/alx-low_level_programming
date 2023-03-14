666d239fd990bda45ce6f661df9e0bd581fef8b5
JoysBoy <tbgselamolela@gmail.com>
2023-03-13 18:45:37 -0700

    main.h update

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,13 +2,24 @@
 #define MAIN_H
 
 int _putchar(char c);
-
-/* Prototypes for functions in file1.c */
-int function1(int arg1, char *arg2);
-void function2(void);
-
-/* Prototypes for functions in file2.c */
-int function3(int arg1);
-char *function4(char *arg2);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
